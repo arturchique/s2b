1fe1bb102ad29aa3191661376bb888268238d6fb
Artur <aepremyan3993@gmail.com>
2020-10-31 02:11:57 +0300

    made first settings

---
+++ s2b/s2b_api/glossary.py
@@ -0,0 +1,45 @@
+ANIMAL_KIND_CHOICES = (
+    ("c", "КОТ"),
+    ("d", "СОБАКА"),
+)
+
+ANIMAL_SEX_CHOICES = (
+    ('m', 'MALE'),
+    ('f', 'FEMALE'),
+)
+
+ANIMAL_BREED_CHOICES = [
+    ("", ""),
+    ("", ""),
+]
+
+ANIMAL_COLOR_CHOICES = [
+    ("", ""),
+    ("", ""),
+]
+
+ANIMAL_HAIR_CHOICES = [
+    ("", ""),
+    ("", ""),
+]
+
+ANIMAL_EAR_CHOICES = [
+    ("", ""),
+    ("", ""),
+]
+
+ANIMAL_TAIL_CHOICES = [
+    ("", ""),
+    ("", ""),
+]
+
+ANIMAL_SIZE_CHOICES = (
+    ('s', 'Малый'),
+    ('m', 'Средний'),
+    ('l', 'Крупный'),
+)
+
+LEAVING_REASON_CHOICES = [
+    ("", ""),
+    ("", ""),
+]
--- s2b/s2b_api/models.py
+++ s2b/s2b_api/models.py
@@ -1,3 +1,166 @@
 from django.db import models
+import uuid
+from django.contrib.auth import get_user_model
+from datetime import datetime
+from .glossary import *
 
-# Create your models here.
+User = get_user_model()
+
+
+class Shelter(models.Model):
+    name = models.CharField(
+        max_length=40, blank=False, verbose_name="Название приюта",
+        help_text="Название приюта", null=False
+    )
+    address = models.CharField(
+        max_length=100, blank=False, verbose_name="Адресс приюта",
+        help_text="Адресс приюта", null=False
+    )
+    prefecture = models.CharField(
+        max_length=80, blank=False, verbose_name="Подчинение",
+        help_text="Подчинение (например: Префектура ЮВАО)", null=False
+    )
+    phone_number = models.CharField(
+        max_length=20, blank=True, verbose_name="Номер телефона",
+        help_text="Номер телефона", null=True
+    )
+
+
+class Animal(models.Model):
+    animal_accounting_card = models.CharField(
+        primary_key=True, max_length=20, blank=False, null=False,
+        help_text="Номер карточки учета животного", unique=True,
+        verbose_name="Карточка учета животного"
+    )
+    kind = models.CharField(
+        max_length=1, choices=ANIMAL_KIND_CHOICES, verbose_name="Вид животного",
+        help_text="Вид животного", blank=False, null=False
+    )
+    birth_date = models.DateTimeField(
+        verbose_name="Возраст, дата рождения", help_text="Возраст, дата рождения",
+        auto_now=False, blank=False, null=False
+    )
+    weight = models.FloatField(max_length=5, verbose_name="Вес", help_text="Вес", blank=False, null=False)
+    name = models.CharField(
+        max_length=20, verbose_name="Кличка", help_text="Кличка", default="без клички",
+        blank=False, null=False
+    )
+    sex = models.CharField(
+        max_length=1, choices=ANIMAL_SEX_CHOICES, verbose_name="Пол", help_text="Пол",
+        blank=True, null=True
+    )
+    breed = models.CharField(
+        max_length=25, choices=ANIMAL_BREED_CHOICES, verbose_name="Порода",
+        blank=False, null=False, default="метис", help_text="Порода"
+    )
+    color = models.CharField(
+        max_length=25, choices=ANIMAL_COLOR_CHOICES, verbose_name="Цвет",
+        blank=False, null=False, default="черный", help_text="Цвет"
+    )
+    hair = models.CharField(
+        max_length=25, choices=ANIMAL_HAIR_CHOICES, verbose_name="Шерсть",
+        blank=False, null=False, default="обычная", help_text="Шерсть"
+    )
+    ears = models.CharField(
+        max_length=25, choices=ANIMAL_EAR_CHOICES, verbose_name="Уши",
+        blank=False, null=False, default="стоячие", help_text="Уши"
+    )
+    tail = models.CharField(
+        max_length=25, choices=ANIMAL_TAIL_CHOICES, verbose_name="Хвост",
+        blank=False, null=False, default="обычный", help_text="Хвост"
+    )
+    size = models.CharField(
+        max_length=1, choices=ANIMAL_SIZE_CHOICES, verbose_name="Размер",
+        blank=False, null=False, default="средний", help_text="Размер"
+    )
+    identifying_marks = models.CharField(
+        max_length=50, default="нет", help_text="Особые приметы",
+        verbose_name="Особые приметы", blank=False, null=False
+    )
+    cage_number = models.IntegerField(
+        verbose_name="Вольер N", help_text="Номер вольера", blank=True, null=True
+    )
+    animal_id = models.BigIntegerField(
+        verbose_name="Идентификационная метка", help_text="Идентификационная метка",
+        unique=True, blank=False, null=False
+    )
+    sterilization_date = models.DateTimeField(
+        auto_now=False, verbose_name="Дата стериллизации", null=False,
+        help_text="Дата стериллизации", blank=False
+    )
+    doctor_name = models.CharField(
+        max_length=50, help_text="ФИО вет. врача", verbose_name="ФИО врача",
+        blank=False, null=False
+    )
+    socialized = models.BooleanField(
+        default=False, verbose_name="Социализировано", help_text="Социализировано (да/нет)",
+        blank=False, null=False
+    )
+    entrance_act = models.CharField(
+        max_length=15, verbose_name="Акт о поступлении", help_text="Акт о поступлении",
+        blank=False, null=False
+    )
+    leaving_act = models.CharField(
+        max_length=15, verbose_name="Акт о выбытии", help_text="Акт о выбытии",
+        blank=True, null=True
+    )
+    entrance_act_date = models.DateTimeField(
+        auto_now=False, verbose_name="Акт о поступлении. Дата", null=False,
+        help_text="Акт о поступлении. Дата", blank=False
+    )
+    leaving_act_date = models.DateTimeField(
+        auto_now=False, verbose_name="Дата выбытия", null=True,
+        help_text="Дата выбытия", blank=True
+    )
+    leaving_act_reason = models.CharField(
+        max_length=1, choices=LEAVING_REASON_CHOICES, verbose_name="Причина выбытия",
+        blank=True, null=True, help_text="Причина выбытия"
+    )
+    region = models.CharField(
+        max_length=10, verbose_name="Административный округ", help_text="Административный округ",
+        blank=False, null=False
+    )
+    catching_act = models.CharField(
+        max_length=15, verbose_name="Акт отлова", help_text="Акт отлова",
+        blank=False, null=False
+    )
+    catching_address = models.CharField(
+        max_length=15, verbose_name="Адрес места отлова", help_text="Адрес места отлова",
+        blank=False, null=False
+    )
+    legal_entity = models.CharField(
+        max_length=15, verbose_name="Юридическое лицо", help_text="Юридическое лицо",
+        blank=True, null=True
+    )
+    owner_name = models.CharField(
+        max_length=15, verbose_name="ФИО опекунов", help_text="ФИО опекунов",
+        blank=True, null=True
+    )
+    person_owner_name = models.CharField(
+        max_length=15, verbose_name="ФИО физического лица", help_text="ФИО физического лица",
+        blank=True, null=True
+    )
+    shelter = models.ForeignKey(
+        Shelter, on_delete=models.CASCADE, help_text="Приют", verbose_name="Приют",
+        blank=False, null=False
+    )
+    staff_name = models.CharField(
+        max_length=15, verbose_name="ФИО сотрудника по уходу", help_text="ФИО сотрудника по уходу",
+        blank=True, null=True
+    )
+    parasites_treatment = models.TextField(
+        verbose_name="Обработка от эндо- и эктопаразитов", blank=True, max_length=500,
+        help_text="Введите данные об обработке от паразитов в формате: дата, название препарата, доза"
+    )
+    vaccinations = models.TextField(
+        verbose_name="Вакцины", blank=True, max_length=500,
+        help_text="Введите данные о вакцинации в формате: дата, вид вакцины, номер серии"
+    )
+    medical_checkup_date = models.DateTimeField(
+        verbose_name="Дата медосмотра", blank=True, auto_now=False,
+        help_text="Дата медосмотра"
+    )
+    anamnesis = models.CharField(
+        verbose_name="Анамнез", blank=True, help_text="Анамнез",
+        null=False, max_length=20
+    )
