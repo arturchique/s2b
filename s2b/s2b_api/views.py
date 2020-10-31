02fb9a1ed67fc08b216f82c3bb0c07ac58017515
Artur <aepremyan3993@gmail.com>
2020-10-31 21:07:46 +0300

    made first settings

--- s2b/s2b_api/views.py
+++ s2b/s2b_api/views.py
@@ -11,14 +11,22 @@ from django.contrib.auth import get_user_model
 
 
 class AdminView(APIView):
-    def get(self, request):
+    def post(self, request):
         user = request.user
         worker = Worker.objects.get(user=user)
         shelter = worker.shelter
         if worker.position == "w":
-            animals = Animal.objects.filter(shelter=shelter)
-            serializer = AnimalSerializer(animals, many=True)
-            return Response({"data": serializer.data})
+            if not request.data["filters"]:
+                animals = Animal.objects.filter(shelter=shelter)
+                paginator = Paginator(animals, 2)
+                page = request.data["page"]
+                paged_listings = paginator.get_page(page)
+                serializer = AnimalSerializer(paged_listings, many=True)
+                return Response({
+                    "data": serializer.data,
+                    "status": "Работник приюта",
+                    "total_page_count": paginator.num_pages
+                })
         elif worker.position == "a":
             return Response({"data": "Это админ приюта"})
         return Response({"data": "ok"})
