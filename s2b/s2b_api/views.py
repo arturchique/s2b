ec1b2e385c9a3b66f2d9dc1032a332afcf99b745
Artur <aepremyan3993@gmail.com>
2020-10-31 23:06:54 +0300

    made first settings

--- s2b/s2b_api/views.py
+++ s2b/s2b_api/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
-from openpyxl import load_workbook
 from rest_framework.response import Response
 from rest_framework import generics, permissions
 from django.core.paginator import Paginator
